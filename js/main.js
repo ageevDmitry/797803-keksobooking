4999530b2f026cb0058bbf4637f8eac1910228cb
DmitryAgeev <ageevdmitry2018@gmail.com>
2018-12-26 18:31:24 +0300

    Выполнил часть задания про валидацию

--- js/main.js
+++ js/main.js
@@ -230,16 +230,31 @@ var adForm = document.querySelector('.ad-form');
 var selectors = document.querySelectorAll(
   'form.ad-form input, form.ad-form  select, form.ad-form  textarea, form.map__filters select, form.map__filters input'
 );
+var titleNotice = document.querySelector('#title');
+var priceNotice = document.querySelector('#price');
+var timeInNotice = document.querySelector('#timein');
+var timeOutNotice = document.querySelector('#timeout');
+var addressNotice = document.querySelector('#address');
 
 var ready = function () {
-  document.querySelector('#address').setAttribute('value', PIC_COORDINATE_Х_DEFAULT + ', ' + PIC_COORDINATE_Y_DEFAULT);
+  addressNotice.setAttribute('value', PIC_COORDINATE_Х_DEFAULT + ', ' + PIC_COORDINATE_Y_DEFAULT);
+  addressNotice.setAttribute('readonly', 'readonly');
 
   selectors.forEach(function (selector) {
     selector.setAttribute('disabled', 'disabled');
   });
-  
+
   adForm.querySelector('.ad-form__element--submit').setAttribute('disabled', 'disabled');
   adForm.querySelector('.features').setAttribute('disabled', 'disabled');
+  titleNotice.setAttribute('required', 'required');
+  titleNotice.setAttribute('minlength', '30');
+  titleNotice.setAttribute('maxlength', '100');
+  priceNotice.setAttribute('required', 'required');
+  priceNotice.setAttribute('max', 1000000);
+  adForm.setAttribute('method', 'post');
+  adForm.setAttribute('enctype', 'multipart/form-data');
+  adForm.setAttribute('action', 'https://js.dump.academy/keksobooking');
+
 };
 
 mapPinMain.addEventListener('mouseup', function () {
@@ -256,4 +271,60 @@ mapPinMain.addEventListener('mouseup', function () {
   });
 });
 
+document.querySelector('#type').addEventListener('change', function (evt) {
+  if (evt.target.value === 'bungalo') {
+    priceNotice.setAttribute('min', 0);
+    priceNotice.setAttribute('placeholder', 0);
+  } else if (evt.target.value === 'flat') {
+    priceNotice.setAttribute('min', 1000);
+    priceNotice.setAttribute('placeholder', 1000);
+  } else if (evt.target.value === 'house') {
+    priceNotice.setAttribute('min', 5000);
+    priceNotice.setAttribute('placeholder', 5000);
+  } else {
+    priceNotice.setAttribute('min', 10000);
+    priceNotice.setAttribute('placeholder', 10000);
+  }
+});
+
+document.querySelector('#timein').addEventListener('change', function (evt) {
+  if (evt.target.value === '12:00') {
+    timeOutNotice.value = '12:00';
+  } else if (evt.target.value === '13:00') {
+    timeOutNotice.value = '13:00';
+  } else {
+    timeOutNotice.value = '14:00';
+  }
+});
+
+document.querySelector('#timeout').addEventListener('change', function (evt) {
+  if (evt.target.value === '12:00') {
+    timeInNotice.value = '12:00';
+  } else if (evt.target.value === '13:00') {
+    timeInNotice.value = '13:00';
+  } else {
+    timeInNotice.value = '14:00';
+  }
+});
+
+
 document.addEventListener('DOMContentLoaded', ready);
+
+// Пока условно создал отображения шаблонов на кпопки для проверки
+// document.querySelector('.ad-form__submit').addEventListener('click', function () {
+
+//  var notice = document.querySelector('.notice')
+//  var successMessage = document.querySelector('#success').content;
+//  var success = successMessage.cloneNode(true);
+
+//  notice.appendChild(success);
+//});
+
+//document.querySelector('.ad-form__reset').addEventListener('click', function () {
+
+//  var notice = document.querySelector('.notice')
+//  var errorMessage = document.querySelector('#error').content;
+//  var error = errorMessage.cloneNode(true);
+
+//  notice.appendChild(error);
+//});
